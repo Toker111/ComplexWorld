2f4431fa3468768810a06d66baf6894448286b68
Toker <3374284195@qq.com>
2026-05-16 16:07:40 +0800

    移动要消耗能量

--- 4 energy_move.py
+++ 4 energy_move.py
@@ -6,7 +6,8 @@ world=[['.' for _ in range(world_size)] for _ in range(world_size)]
 
 
 def people():
-    global life_x,life_y
+    global life_x,life_y,energy
+    energy=100
     life_x,life_y=random.randint(0,world_size-1),random.randint(0,world_size-1)
     while world[life_y][life_x]!='.':
         life_x, life_y = random.randint(0, world_size - 1), random.randint(0, world_size - 1)
@@ -41,9 +42,10 @@ def gaol_move(food_x,food_y):
 
     move_x,move_y=0,0
 
-    global life_x, life_y,n
+    global life_x, life_y,n,energy
     for step in range(1000):
         if world[food_y][food_x]==world[life_y][life_x]:
+            energy+=30
             print(f'总共{n}')
             print('========吃到了，开心心=========')
             # ==========移除被吃掉的食物===========
@@ -59,11 +61,12 @@ def gaol_move(food_x,food_y):
         life_x+=move_x
         life_y+=move_y
         world[life_y][life_x]='0'
+        energy-=5
         n+=1
         print(f'======这是第{n}步=======')
         look(world)
 def move():
-    global life_x, life_y
+    global life_x, life_y,energy
     move_x,move_y=random.choice([(0,1),(0,-1),(1,0),(-1,0)])
     new_x=life_x+move_x
     new_y=life_y+move_y
@@ -71,7 +74,9 @@ def move():
     new_y=max(0,min(new_y,world_size-1))
     world[life_y][life_x]='.'
     world[new_y][new_x]='0'
+    energy -= 5
     life_x,life_y=new_x,new_y
+    look(world)
 look(world)
 vision_range=4
 
@@ -82,6 +87,9 @@ look(world)
 
 for i in range(100):
     visible_food = []
+    if energy<0:
+        print('========死亡是不可避免的，期待王的新生========')
+        break
     if not food_position:
         break
     for x, y in food_position:
